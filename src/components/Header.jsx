4587ab4f7c9bfa62e6407e1fff13811af286960c
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2024-03-31 23:34:58 -0400

    base styles for body and other selectors added. Headerand Mobile Nav created

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+
+const Header = () => {
+    return (
+
+        <header>
+            <div className="header-grid">
+                <div className="header-image">
+
+                </div>
+                <div className="header-content">
+                    <h2>Oh Hi, Im Alex</h2>
+                    <h2></h2>
+                </div>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header;
