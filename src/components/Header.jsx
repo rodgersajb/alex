56adad8f183352652e2983767066afb867725f4b
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2024-04-02 10:47:22 -0400

    added base styles for buttons, adjusted html for headings, hover and focus effects added for buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
 import MobileNav from "./MobileNav";
 
-import Alex from "../assets/alex-blur-orange.jpg";
+import Alex from "../assets/alex2.png";
+
+import { FaHandPointDown } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header>
       <div className="header-grid">
         <div className="header-content">
-          <h2>Oh Hi, </h2>
-          <h2>I`m Alex</h2>
-          <h2 className="highlighter-accent cursive-text">Web Developer</h2>
+          <div className="headings-container">
+            <h2>Oh Hi, </h2>
+            <div className="flex">
+              <h2 className="highlighter-accent">I`m Alex</h2>
+              <FaHandPointDown />
+            </div>
+
+            <h2 className="highlighter-accent cursive-text">Web Developer</h2>
+          </div>
+
+          <div className="header-buttons">
+            <button>Browse Project</button>
+            <button>Get in Touch</button>
+          </div>
         </div>
         <div className="header-image">
           <img src={Alex} alt="" />
